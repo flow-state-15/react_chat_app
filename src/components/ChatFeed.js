fcfcb1edf6337937a65e7f392c08020b6527c179
flow-state-15 <purcellgroup88@gmail.com>
2021-11-02 17:01:25 -0500

    progress so far

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -3,7 +3,29 @@ import { MyMessage } from './MyMessage'
 import { TheirMessage } from './TheirMessage'
 
 export const ChatFeed = (props) => {
-    console.log(props)
+    const { chats, activeChat, userName, messages } = props;
+
+    const chat = chats && chats[activeChat]
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages)
+
+        return keys.map((key, index => {
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : keys[index - 1]
+            const isMyMessage = userName === message.sender.username;
+
+            return (
+                <div key={'msg_${index}'} style={{ width: '100%'}}>
+                    <div className="message-block">
+
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    renderMessages()
 
     return (
         <div>
